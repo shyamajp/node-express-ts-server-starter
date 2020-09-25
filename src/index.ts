116ddd26d376d2c77477ee49632b0553c9540d2e
shyamajp <shyamajp@users.noreply.github.com>
2020-09-26 00:17:02 +0300

    add mongodb connection

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,18 @@
 import express from 'express';
 import bodyParser from 'body-parser';
+import mongoose from 'mongoose';
 require('dotenv').config();
 
 // use PORT inside /.env, if not, use 3000
 const port = process.env.PORT || 3000;
+// use MONGO_URI inside /.env, if not, use default localhost uri
+const mongo_uri: string = process.env.MONGO_URI || "mongodb://localhost:27017/?readPreference=primary&appname=MongoDB%20Compass%20Community&ssl=false";
+mongoose.connect(mongo_uri, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useFindAndModify: false,
+    useCreateIndex: true
+});
 
 const app = express();
 
