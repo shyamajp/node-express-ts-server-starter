c4e70c9383ac21b56aeafc201ba9690a73ace316
Shiori Yamazaki <shiori.yamazaki.jp@gmail.com>
2021-12-26 22:47:48 +0200

    update packages

--- src/index.ts
+++ src/index.ts
@@ -14,5 +14,5 @@ applyMiddleware(errorHandlers, app);
 
 const PORT: string | number = process.env.PORT || 3000;
 app.listen(PORT, () => {
-    console.log(`Server is running at http://localhost:${PORT}`);
+  console.log(`Server is running at http://localhost:${PORT}`);
 });
